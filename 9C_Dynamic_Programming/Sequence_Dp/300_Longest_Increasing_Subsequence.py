390065693e51d21a4f38d8f2c0e8619e5dd83083
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-03 11:15:47 -0400

    Update and rename NOTSTART_300_Longest_Increasing_Subsequence.py to 300_Longest_Increasing_Subsequence.py

--- 9C_Dynamic_Programming/Sequence_Dp/300_Longest_Increasing_Subsequence.py
+++ 9C_Dynamic_Programming/Sequence_Dp/300_Longest_Increasing_Subsequence.py
@@ -13,5 +13,16 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 '''
 
-
-
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        n = len(nums)
+        dp = [1]*n
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        # print(dp)
+        return max(dp)
+                
