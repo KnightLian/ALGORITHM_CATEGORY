5ac7ee7058182feeca12b827e7e8edd5aa8d6d22
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-03 23:39:50 -0400

    Update NOTFIND_Longest_Common_Substring.py

--- 9C_Dynamic_Programming/Two_Sequences_DP/NOTFIND_Longest_Common_Substring.py
+++ 9C_Dynamic_Programming/Two_Sequences_DP/NOTFIND_Longest_Common_Substring.py
@@ -1,4 +1,17 @@
 '''
+Given two strings, find the longest common substring.
+Return the length of it.
+
+Example 1:
+	Input:  "ABCD" and "CBCE"
+	Output:  2
+	Explanation:
+	Longest common substring is "BC"
 
 
+Example 2:
+	Input: "ABCD" and "EACB"
+	Output:  1
+	Explanation: 
+	Longest common substring is 'A' or 'C' or 'B'
 '''
