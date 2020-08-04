c4e6108c708574504b68d22cac6a0ce33cade871
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-04 00:13:25 -0400

    Update and rename NOTFIND_Minimum_Adjustment_Cost.py to LC91_Minimum_Adjustment_Cost.py

---
+++ 9C_Dynamic_Programming/Backpack/LC91_Minimum_Adjustment_Cost.py
@@ -0,0 +1,17 @@
+'''
+Given an integer array, 
+adjust each integers so that the difference of every adjacent integers are not greater than a given number target.
+If the array before adjustment is A, 
+the array after adjustment is B, you should minimize the sum of |A[i]-B[i]|
+
+-You can assume each number in the array is a positive integer and not greater than 100.
+
+Example 1:
+	Input:  [1,4,2,3], target=1
+	Output:  2
+
+Example 2:
+	Input:  [3,5,4,7], target=2
+	Output:  1
+'''
+
