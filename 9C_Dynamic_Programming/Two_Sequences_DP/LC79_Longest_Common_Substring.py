47eb6155c84ce3427080f7449408c0810001ec04
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-04 10:02:16 -0400

    Update LC79_Longest_Common_Substring.py

--- 9C_Dynamic_Programming/Two_Sequences_DP/LC79_Longest_Common_Substring.py
+++ 9C_Dynamic_Programming/Two_Sequences_DP/LC79_Longest_Common_Substring.py
@@ -23,5 +23,19 @@ class Solution:
     @return: the length of the longest common substring.
     """
     def longestCommonSubstring(self, A, B):
-		
-		
+        n1 = len(A)
+        n2 = len(B)
+        
+        dp = [[0]*(n2+1) for _ in range(n1+1)]
+    
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                if A[i-1] == B[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = 0
+        temp = 0
+        for row in dp:
+            temp = max(temp, max(row))
+        # print(dp)
+        return temp
