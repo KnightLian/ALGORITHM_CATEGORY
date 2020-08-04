1e3bbd4be21ffae68b177ef995a53e96c084f10b
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-03 23:35:39 -0400

    Update and rename CURRENT_1143_Longest_Common_Subsequence.py to 1143_Longest_Common_Subsequence.py

--- 9C_Dynamic_Programming/Two_Sequences_DP/1143_Longest_Common_Subsequence.py
+++ 9C_Dynamic_Programming/Two_Sequences_DP/1143_Longest_Common_Subsequence.py
@@ -30,3 +30,21 @@ Constraints:
 The input strings consist of lowercase English characters only.
 '''
 
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
+        dp = [[0]*(n2+1) for _ in range(n1+1)]
+
+        #################
+        #################
+        #################
+
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+                    
+        return dp[-1][-1]
