6561eac585897bc766fca46828adac24b4a5dfe3
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-04 00:15:57 -0400

    Update and rename NOTFIND_k_Sum.py to LC89_k_Sum.py

---
+++ 9C_Dynamic_Programming/Backpack/LC89_k_Sum.py
@@ -0,0 +1,21 @@
+'''
+Given n distinct positive integers, integer k (k <= n) and a number target.
+Find k numbers where sum is target. Calculate how many solutions there are?
+
+Example 1
+Input:
+List = [1,2,3,4]
+k = 2
+target = 5
+Output: 2
+Explanation: 1 + 4 = 2 + 3 = 5
+Example 2
+
+Input:
+List = [1,2,3,4,5]
+k = 3
+target = 6
+Output: 1
+Explanation: There is only one method. 1 + 2 + 3 = 6
+'''
+
