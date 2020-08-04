23b4b53d702857f8c4f44ea648ce4027326eae74
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-04 00:05:49 -0400

    Update and rename NOTFIND_Backpack.py to LC92_Backpack.py

---
+++ 9C_Dynamic_Programming/Backpack/LC92_Backpack.py
@@ -0,0 +1,16 @@
+'''
+Given n items with size Ai, 
+an integer m denotes the size of a backpack. How full you can fill this backpack?
+
+Example 1:
+	Input:  [3,4,8,5], backpack size=10
+	Output:  9
+
+Example 2:
+	Input:  [2,3,5,7], backpack size=12
+	Output:  12
+  
+O(n x m) time and O(m) memory.
+O(n x m) memory is also acceptable if you do not know how to optimize memory.
+'''
+
