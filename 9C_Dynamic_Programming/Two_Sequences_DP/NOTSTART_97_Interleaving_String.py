a6f224e035ca78c3b440b88b68d8233acb5baff8
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-06 09:59:34 -0400

    Update NOTSTART_97_Interleaving_String.py

--- 9C_Dynamic_Programming/Two_Sequences_DP/NOTSTART_97_Interleaving_String.py
+++ 9C_Dynamic_Programming/Two_Sequences_DP/NOTSTART_97_Interleaving_String.py
@@ -10,3 +10,31 @@ Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
 '''
 
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1+n2 != len(s3):
+            return False
+
+        dp = [[False]*(n2+1) for _ in range(n1+1)]       
+        dp[0][0] = True
+
+        for i in range(1, n2+1):
+            if s2[:(i-1+1)] == s3[:(i-1+1)]:
+                dp[0][i] = True
+        for i in range(1, n1+1):
+            if s1[:(i-1+1)] == s3[:(i-1+1)]:
+                dp[i][0] = True
+        # print(dp)
+        
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                if s1[i-1] == s3[i+j-1] and dp[i-1][j] == True:
+                    dp[i][j] = True
+                elif s2[j-1] == s3[i+j-1] and dp[i][j-1] == True:
+                    dp[i][j] = True
+
+        return dp[-1][-1]
+      
