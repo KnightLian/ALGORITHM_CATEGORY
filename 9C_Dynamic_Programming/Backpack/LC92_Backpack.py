11ee5a8c3c975bc588745373555c071d147c240c
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-09 23:13:37 -0400

    Update LC92_Backpack.py

--- 9C_Dynamic_Programming/Backpack/LC92_Backpack.py
+++ 9C_Dynamic_Programming/Backpack/LC92_Backpack.py
@@ -16,3 +16,32 @@ O(n x m) time and O(m) memory.
 O(n x m) memory is also acceptable if you do not know how to optimize memory.
 '''
 
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @return: The maximum size
+    """
+    def backPack(self, m, A):
+        # write your code here
+        n = len(A)
+        dp = [[0]*(m+1) for _ in range(n)]
+        
+        #initialize 1st weight
+        for i in range(m+1):
+            if i >= A[0]:
+                dp[0][i] = A[0]
+        
+        for i in range(1, n):
+            for j in range(1, m+1):
+                # if weight constrain is bigger 
+                # consider to choose iterm i or not                
+                if j >= A[i]:
+                    dp[i][j] = max(dp[i-1][j],dp[i-1][j-A[i]]+A[i])
+                # otherwise    
+                # have to not choose iterm i                       
+                else:
+                    dp[i][j] = dp[i-1][j]
+                    
+        #print(dp)
+        return dp[-1][-1]
