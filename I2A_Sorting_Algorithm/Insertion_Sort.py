8b4fb691cd5e09fef21ae6260457e76af7c79484
Lian <47261500+KnightLian@users.noreply.github.com>
2020-08-12 00:20:40 -0400

    Update Insertion_Sort.py

--- I2A_Sorting_Algorithm/Insertion_Sort.py
+++ I2A_Sorting_Algorithm/Insertion_Sort.py
@@ -15,3 +15,14 @@ Constraints:
 -50000 <= nums[i] <= 50000
 '''
 
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(1, n):
+            for j in range(i,0,-1):
+                # print(j-1, j)
+                if nums[j-1] > nums[j]:
+                    nums[j-1], nums[j] = nums[j], nums[j-1]
+                else:
+                    break
+        return nums
