cb046c1410d7571f035f4748a4c2364f76aa557e
Lian <47261500+KnightLian@users.noreply.github.com>
2020-09-02 14:46:40 +0800

    Update Maximum_Depth_of_Binary_Tree.py

--- 9C_Binary_Tree/Divide_and_Conquer/Maximum_Depth_of_Binary_Tree.py
+++ 9C_Binary_Tree/Divide_and_Conquer/Maximum_Depth_of_Binary_Tree.py
@@ -1,7 +1,25 @@
 '''
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path 
+from the root node down to the farthest leaf node.
 
+The answer will not exceed 5000
 
+Example 1:
+Input: tree = {}
+Output: 0
+Explanation: The height of empty tree is 0.
 
+Example 2:
+Input: tree = {1,2,3,#,#,4,5}
+Output: 3	
+Explanation: Like this:
+   1
+  / \                
+ 2   3                
+    / \                
+   4   5
+it will be serialized {1,2,3,#,#,4,5}
 '''
 
 
